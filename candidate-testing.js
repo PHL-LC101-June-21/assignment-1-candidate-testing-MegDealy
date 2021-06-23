7dff07f5dba163e70a5861555150afbadc7b82dc
mdealy <85583859+MegDealy@users.noreply.github.com>
2021-06-23 18:13:22 +0000

    Began question functions- working out storing user answers and comparing their values against correct answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,28 +3,37 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = input.question("Hello! What is your name, please?  ");
+let candidateName = input.question("Hello, what is your name?");
+
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
+let question = "Who was the first American woman in space?";
 let correctAnswer;
-let candidateAnswer;
+let candidateAnswer ;
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
-function askForName(){
-  let message ='Welcome, ("candidateName")' 
+function askForName()
+{
+  let message ='Welcome, ' + candidateName + ". I am going to ask you a few questions."
+
+  console.log(message);
+
+  
+
   // TODO 1.1b: Ask for candidate's name //
 }
 
 function askQuestion() {
-  message = "hi (Candidate name" 
+  messages = question  
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log(messages);
 
 }
+
+
 
 function gradeQuiz(candidateAnswers) {
 
